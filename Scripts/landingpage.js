b987dca32e8aeb8f5d3615732a3cb6b5a3e7eb2f
Esther Kariuki <kariukie@whitman.edu>
2026-07-09 00:59:38 -0700

    added pages

---
+++ Scripts/landingpage.js
@@ -0,0 +1,9 @@
+const btn1 = document.getElementById("btn1")
+const audio_list = document.createElement("audio_list");
+audio_list.src = "/lab_audio";
+audio_list.
+
+btn1.addEventListener("click", ( select_audio));
+select_audio(){
+    document.getElementById("audio_list")
+}
